e289814b377b18db8d77ad72f052c471bad99014
Mirgubad <migubad@code.edu.az>
2022-05-24 11:49:22 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp36/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    class Person
+    {
+
+        public Person (string name,string surname,int age)
+        {
+            IsAdult = true;
+            Name = name;
+            SurName = surname;
+            Age = age;
+
+            if (Age < 18)
+            {
+                Console.WriteLine(IsAdult = false);
+            }
+            else
+            {
+                Console.WriteLine(IsAdult);
+            }
+        }
+        
+        public string Name;
+        public string SurName;
+        public int  Age;
+        public bool IsAdult;
+    
+    
+    
+      
+    }
+
+    
+
+
+
+
+
+}
+
+
+
+
---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person MyPerson = new Person("Huseyn", "Eliyev", 17) 
+            { 
+            
+            };
+
+           
+
+        
+        
+        }
+    }
+}
